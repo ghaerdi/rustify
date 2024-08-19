376aca536f30ad2f7cbc2f366b3fddac9fc89d94
ghaerdi <ghaerdi@outlook.com>
2024-08-19 18:49:53 -0400

    feat: add division function with error handling

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+// example of rustify
+import { type Result, Ok, Err } from "./src";
+
+const divide = (a: number, b: number): Result<number, string> =>
+  b === 0 ? Err("Divisor is zero") : Ok(a / b);
+
+const result = divide(10, 5).unwrapOrElse((err) => {
+  console.error("Division error: ", err);
+  return 0;
+});
+
+console.log(result); // 2 or 0 if error
